34f917e4bf984ad190899ba0736b2ca827a57ee9
joelthegoat21 <729037@student.dusd.net>
2025-05-02 15:27:54 -0700

    i added  audio
    today i added audio for my game and now you  can hear the game now

--- P8U3SoundAndEffectPineiro/Assets/Scripts/PlayerController.cs
+++ P8U3SoundAndEffectPineiro/Assets/Scripts/PlayerController.cs
@@ -7,10 +7,14 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private Animator playerAnim;
     public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpsound;
+    public AudioClip crashSound;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,18 +30,20 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
             isOnGround = false;
-            playerAnim.SetTrigger("Jump_trig"); 
+            playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
         }
    
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        
 
         if(collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         } else if(collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Game Over");
@@ -45,6 +51,8 @@ public class PlayerController : MonoBehaviour
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
             explosionParticle.Play();
+            dirtParticle.Stop();
+            
         }
     }
 }
